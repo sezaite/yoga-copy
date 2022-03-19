ee237e6c5d42e5a044e13d8b7e2fa2886dd6d655
martyna <martyna@imas.lt>
2022-03-19 05:00:28 +0200

    span fixed

--- src/components/banners/TopBanner.js
+++ src/components/banners/TopBanner.js
@@ -5,7 +5,7 @@ const TopBanner = () => {
         
                     <div className="d-flex justify-content-center top-banner">
                         <img src={tag} alt="tag icon" />
-                        <p>50% discount only valid for <span> 00:15:49</span></p>
+                        <p>50% discount only valid for <span className="bold"> 00:15:49</span></p>
                     </div>
              
      );
